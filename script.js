333a50f401417f38c175774ebe4873d83a37fee0
Abhishek Anshu <abhianshu2020@gmail.com>
2020-12-15 12:08:20 +0530

    candles added

--- script.js
+++ script.js
@@ -206,36 +206,174 @@ timeline.add(
 
 //=========== Script for Star JS end ===========
 
-//=========== Script for Made with love ===========
 
-$("body").scroll(function () {
-  console.log("helo");
 
-  if ($(".ml16").isVisible()) {
-    console.log("uo");
+//=========== Script for Fireworks ===========
+
+// helper functions
+const PI2 = Math.PI * 2;
+const random = (min, max) => (Math.random() * (max - min + 1) + min) | 0;
+const timestamp = (_) => new Date().getTime();
+
+// container
+class Birthday {
+  constructor() {
+    this.resize();
+
+    // create a lovely place to store the firework
+    this.fireworks = [];
+    this.counter = 0;
   }
-});
 
-// Wrap every letter in a span
-// var textWrapper = document.querySelector('.ml16');
-// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+  resize() {
+    this.width = canvas.width = window.innerWidth;
+    let center = (this.width / 2) | 0;
+    this.spawnA = (center - center / 4) | 0;
+    this.spawnB = (center + center / 4) | 0;
 
-// anime.timeline({loop: true})
-//   .add({
-//     targets: '.ml16 .letter',
-//     translateY: [-100,0],
-//     easing: "easeOutExpo",
-//     duration: 1400,
-//     delay: (el, i) => 30 * i
-//   }).add({
-//     targets: '.ml16',
-//     opacity: 0,
-//     duration: 1000,
-//     easing: "easeOutExpo",
-//     delay: 1000
-//   });
+    this.height = canvas.height = window.innerHeight;
+    this.spawnC = this.height * 0.1;
+    this.spawnD = this.height * 0.5;
+  }
 
-//=========== Script for Made with Love end ===========
+  onClick(evt) {
+    let x = evt.clientX || (evt.touches && evt.touches[0].pageX);
+    let y = evt.clientY || (evt.touches && evt.touches[0].pageY);
+
+    let count = random(3, 5);
+    for (let i = 0; i < count; i++)
+      this.fireworks.push(
+        new Firework(
+          random(this.spawnA, this.spawnB),
+          this.height,
+          x,
+          y,
+          random(0, 260),
+          random(30, 110)
+        )
+      );
+
+    this.counter = -1;
+  }
+
+  update(delta) {
+    ctx.globalCompositeOperation = "hard-light";
+    ctx.fillStyle = `rgba(20,20,20,${7 * delta})`;
+    ctx.fillRect(0, 0, this.width, this.height);
+
+    ctx.globalCompositeOperation = "lighter";
+    for (let firework of this.fireworks) firework.update(delta);
+
+    // if enough time passed... create new new firework
+    this.counter += delta * 3; // each second
+    if (this.counter >= 1) {
+      this.fireworks.push(
+        new Firework(
+          random(this.spawnA, this.spawnB),
+          this.height,
+          random(0, this.width),
+          random(this.spawnC, this.spawnD),
+          random(0, 360),
+          random(30, 110)
+        )
+      );
+      this.counter = 0;
+    }
+
+    // remove the dead fireworks
+    if (this.fireworks.length > 1000)
+      this.fireworks = this.fireworks.filter((firework) => !firework.dead);
+  }
+}
+
+class Firework {
+  constructor(x, y, targetX, targetY, shade, offsprings) {
+    this.dead = false;
+    this.offsprings = offsprings;
+
+    this.x = x;
+    this.y = y;
+    this.targetX = targetX;
+    this.targetY = targetY;
+
+    this.shade = shade;
+    this.history = [];
+  }
+  update(delta) {
+    if (this.dead) return;
+
+    let xDiff = this.targetX - this.x;
+    let yDiff = this.targetY - this.y;
+    if (Math.abs(xDiff) > 3 || Math.abs(yDiff) > 3) {
+      // is still moving
+      this.x += xDiff * 2 * delta;
+      this.y += yDiff * 2 * delta;
+
+      this.history.push({
+        x: this.x,
+        y: this.y,
+      });
+
+      if (this.history.length > 20) this.history.shift();
+    } else {
+      if (this.offsprings && !this.madeChilds) {
+        let babies = this.offsprings / 2;
+        for (let i = 0; i < babies; i++) {
+          let targetX =
+            (this.x + this.offsprings * Math.cos((PI2 * i) / babies)) | 0;
+          let targetY =
+            (this.y + this.offsprings * Math.sin((PI2 * i) / babies)) | 0;
+
+          birthday.fireworks.push(
+            new Firework(this.x, this.y, targetX, targetY, this.shade, 0)
+          );
+        }
+      }
+      this.madeChilds = true;
+      this.history.shift();
+    }
+
+    if (this.history.length === 0) this.dead = true;
+    else if (this.offsprings) {
+      for (let i = 0; this.history.length > i; i++) {
+        let point = this.history[i];
+        ctx.beginPath();
+        ctx.fillStyle = "hsl(" + this.shade + ",100%," + i + "%)";
+        ctx.arc(point.x, point.y, 1, 0, PI2, false);
+        ctx.fill();
+      }
+    } else {
+      ctx.beginPath();
+      ctx.fillStyle = "hsl(" + this.shade + ",100%,50%)";
+      ctx.arc(this.x, this.y, 1, 0, PI2, false);
+      ctx.fill();
+    }
+  }
+}
+
+let canvas = document.getElementById("birthday");
+let ctx = canvas.getContext("2d");
+
+let then = timestamp();
+
+let birthday = new Birthday();
+window.onresize = () => birthday.resize();
+document.onclick = (evt) => birthday.onClick(evt);
+document.ontouchstart = (evt) => birthday.onClick(evt);
+(function loop() {
+  requestAnimationFrame(loop);
+
+  let now = timestamp();
+  let delta = now - then;
+
+  then = now;
+  birthday.update(delta / 1000);
+})();
+
+//=========== Script for Fireworks end ===========
+
+
+//=========== Script for Made with love ===========
 
 $.fn.isInViewport = function () {
   var elementTop = $(this).offset().top;
@@ -251,8 +389,18 @@ $(function () {
     if ($(".ml16").isInViewport()) {
       if (!animated) {
         // Wrap every letter in a span
-        var textWrapper = document.querySelector(".ml16");
-        textWrapper.innerHTML = textWrapper.textContent.replace(
+        const textWrapper = document.querySelectorAll(".ml16");
+        textWrapper[0].innerHTML = textWrapper[0].textContent.replace(
+          /\S/g,
+          "<span class='letter'>$&</span>"
+        );
+
+        textWrapper[1].innerHTML = textWrapper[1].textContent.replace(
+          /\S/g,
+          "<span class='letter'>$&</span>"
+        );
+
+        textWrapper[2].innerHTML = textWrapper[2].textContent.replace(
           /\S/g,
           "<span class='letter'>$&</span>"
         );
@@ -280,3 +428,17 @@ $(function () {
 
   $(".tlt").textillate();
 });
+
+
+//=========== Script for Made with Love end ===========
+
+// const shine = new Shine(document.getElementById('happy-birthday'));
+
+// window.addEventListener('mousemove', function(event) {
+//   shine.light.position.x = event.clientX;
+//   shine.light.position.y = event.clientY;
+//   shine.draw();
+// }, false);
+
+
+
