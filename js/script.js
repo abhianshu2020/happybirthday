9089b34c632ff9294a484b1995baeaf637f51eff
Abhishek Anshu <abhianshu2020@gmail.com>
2020-12-17 00:45:30 +0530

    birthday audio added, fireworks fixed, candles page uplifted

--- js/script.js
+++ js/script.js
@@ -206,173 +206,6 @@ timeline.add(
 
 //=========== Script for Star JS end ===========
 
-
-
-// //=========== Script for Fireworks ===========
-
-// // helper functions
-// const PI2 = Math.PI * 2;
-// const random = (min, max) => (Math.random() * (max - min + 1) + min) | 0;
-// const timestamp = (_) => new Date().getTime();
-
-// // container
-// class Birthday {
-//   constructor() {
-//     this.resize();
-
-//     // create a lovely place to store the firework
-//     this.fireworks = [];
-//     this.counter = 0;
-//   }
-
-//   resize() {
-//     this.width = canvas.width = window.innerWidth;
-//     let center = (this.width / 2) | 0;
-//     this.spawnA = (center - center / 4) | 0;
-//     this.spawnB = (center + center / 4) | 0;
-
-//     this.height = canvas.height = window.innerHeight;
-//     this.spawnC = this.height * 0.1;
-//     this.spawnD = this.height * 0.5;
-//   }
-
-//   onClick(evt) {
-//     let x = evt.clientX || (evt.touches && evt.touches[0].pageX);
-//     let y = evt.clientY || (evt.touches && evt.touches[0].pageY);
-
-//     let count = random(3, 5);
-//     for (let i = 0; i < count; i++)
-//       this.fireworks.push(
-//         new Firework(
-//           random(this.spawnA, this.spawnB),
-//           this.height,
-//           x,
-//           y,
-//           random(0, 260),
-//           random(30, 110)
-//         )
-//       );
-
-//     this.counter = -1;
-//   }
-
-//   update(delta) {
-//     ctx.globalCompositeOperation = "hard-light";
-//     ctx.fillStyle = `rgba(20,20,20,${7 * delta})`;
-//     ctx.fillRect(0, 0, this.width, this.height);
-
-//     ctx.globalCompositeOperation = "lighter";
-//     for (let firework of this.fireworks) firework.update(delta);
-
-//     // if enough time passed... create new new firework
-//     this.counter += delta * 3; // each second
-//     if (this.counter >= 1) {
-//       this.fireworks.push(
-//         new Firework(
-//           random(this.spawnA, this.spawnB),
-//           this.height,
-//           random(0, this.width),
-//           random(this.spawnC, this.spawnD),
-//           random(0, 360),
-//           random(30, 110)
-//         )
-//       );
-//       this.counter = 0;
-//     }
-
-//     // remove the dead fireworks
-//     if (this.fireworks.length > 1000)
-//       this.fireworks = this.fireworks.filter((firework) => !firework.dead);
-//   }
-// }
-
-// class Firework {
-//   constructor(x, y, targetX, targetY, shade, offsprings) {
-//     this.dead = false;
-//     this.offsprings = offsprings;
-
-//     this.x = x;
-//     this.y = y;
-//     this.targetX = targetX;
-//     this.targetY = targetY;
-
-//     this.shade = shade;
-//     this.history = [];
-//   }
-//   update(delta) {
-//     if (this.dead) return;
-
-//     let xDiff = this.targetX - this.x;
-//     let yDiff = this.targetY - this.y;
-//     if (Math.abs(xDiff) > 3 || Math.abs(yDiff) > 3) {
-//       // is still moving
-//       this.x += xDiff * 2 * delta;
-//       this.y += yDiff * 2 * delta;
-
-//       this.history.push({
-//         x: this.x,
-//         y: this.y,
-//       });
-
-//       if (this.history.length > 20) this.history.shift();
-//     } else {
-//       if (this.offsprings && !this.madeChilds) {
-//         let babies = this.offsprings / 2;
-//         for (let i = 0; i < babies; i++) {
-//           let targetX =
-//             (this.x + this.offsprings * Math.cos((PI2 * i) / babies)) | 0;
-//           let targetY =
-//             (this.y + this.offsprings * Math.sin((PI2 * i) / babies)) | 0;
-
-//           birthday.fireworks.push(
-//             new Firework(this.x, this.y, targetX, targetY, this.shade, 0)
-//           );
-//         }
-//       }
-//       this.madeChilds = true;
-//       this.history.shift();
-//     }
-
-//     if (this.history.length === 0) this.dead = true;
-//     else if (this.offsprings) {
-//       for (let i = 0; this.history.length > i; i++) {
-//         let point = this.history[i];
-//         ctx.beginPath();
-//         ctx.fillStyle = "hsl(" + this.shade + ",100%," + i + "%)";
-//         ctx.arc(point.x, point.y, 1, 0, PI2, false);
-//         ctx.fill();
-//       }
-//     } else {
-//       ctx.beginPath();
-//       ctx.fillStyle = "hsl(" + this.shade + ",100%,50%)";
-//       ctx.arc(this.x, this.y, 1, 0, PI2, false);
-//       ctx.fill();
-//     }
-//   }
-// }
-
-// let canvas = document.getElementById("birthday");
-// let ctx = canvas.getContext("2d");
-
-// let then = timestamp();
-
-// let birthday = new Birthday();
-// window.onresize = () => birthday.resize();
-// document.onclick = (evt) => birthday.onClick(evt);
-// document.ontouchstart = (evt) => birthday.onClick(evt);
-// (function loop() {
-//   requestAnimationFrame(loop);
-
-//   let now = timestamp();
-//   let delta = now - then;
-
-//   then = now;
-//   birthday.update(delta / 1000);
-// })();
-
-//=========== Script for Fireworks end ===========
-
-
 //=========== Script for Made with love ===========
 
 $.fn.isInViewport = function () {
@@ -383,8 +216,18 @@ $.fn.isInViewport = function () {
   return elementBottom > viewportTop && elementTop < viewportBottom;
 };
 
+let bomb_interval1 = 0;
+let bomb_interval2 = 0;
+let isBombRunning = false;
+
+let hbdAudio = new Audio("./audio/hbd-bells.mp3");
+let shouldAudioPlay = true;
+
 $(function () {
   let animated = false;
+  let candleTextAnimated = false;
+  let bdayExplosionAnimated = false;
+
   $(window).on("resize scroll", function () {
     if ($(".ml16").isInViewport()) {
       if (!animated) {
@@ -400,11 +243,6 @@ $(function () {
           "<span class='letter'>$&</span>"
         );
 
-        textWrapper[2].innerHTML = textWrapper[2].textContent.replace(
-          /\S/g,
-          "<span class='letter'>$&</span>"
-        );
-
         anime
           .timeline({ loop: false })
           .add({
@@ -412,7 +250,7 @@ $(function () {
             translateY: [-100, 0],
             easing: "easeInExpo",
             duration: 1400,
-            delay: (el, i) => 30 * i,
+            delay: (el, i) => 35 * i,
           })
           .add({
             targets: ".ml16",
@@ -421,13 +259,134 @@ $(function () {
             easing: "easeInExpo",
             delay: 1000,
           });
-          animated = !animated
+        animated = !animated;
       }
     }
-  });
 
-  $(".tlt").textillate();
+    if ($(".ml3").isInViewport()) {
+      if (!candleTextAnimated) {
+        const candleTextWrapper = document.querySelector(".ml3");
+        candleTextWrapper.innerHTML = candleTextWrapper.textContent.replace(
+          /\S/g,
+          "<span class='letter'>$&</span>"
+        );
+
+        anime
+          .timeline({ loop: false })
+          .add({
+            targets: ".ml3 .letter",
+            opacity: [0, 1],
+            easing: "easeInOutQuad",
+            duration: 1000,
+            delay: (el, i) => 150 * (i + 1),
+          })
+          .add({
+            targets: ".ml3",
+            opacity: 1,
+            duration: 1000,
+            easing: "easeInExpo",
+            delay: 1000,
+          });
+          candleTextAnimated = !candleTextAnimated
+      }
+    }
+
+    // ================
+
+    if ($(".happy-birthday-text").isInViewport && !bdayExplosionAnimated) {
+      console.log("first explosion started");
+      for (let i = 1; i < 50; i++)
+        setTimeout(() => {
+          startDoubleExplosion(
+            document.body.clientWidth / 2,
+            window.innerHeight / 2
+          );
+        }, i * 0.1 * 1000);
+
+      for (let i = 1; i < 25; i++) {
+        const x = randomFloat(100, document.body.clientWidth);
+        const y = randomFloat(100, window.innerHeight / 2);
+
+        setTimeout(() => {
+          startDoubleExplosion(x, y);
+        }, 50 * 0.1 * 1000);
+      }
+
+      bdayExplosionAnimated = !bdayExplosionAnimated;
+    }
+
+    if (!$(".happy-birthday-text").isInViewport()) {
+      debugger;
+      console.log("Element is NOT visible. is bomb running: ", isBombRunning);
+      if (isBombRunning) {
+        console.log("interval stopped");
+        clearInterval(bomb_interval1);
+        clearInterval(bomb_interval2);
+        isBombRunning = false;
+
+        shouldPlayAudio = false;
+        hbdAudio.loop = false;
+      }
+    }
+
+    if ($(".happy-birthday-text").isInViewport()) {
+      // code to blast firework at random intervals
+      debugger;
+      console.log("Element is visible. is bomb running: ", isBombRunning);
+      if (!isBombRunning) {
+        console.log("interval started");
+        // setTimeout(() => {
+        bomb_interval1 = setInterval(() => {
+          const x = randomFloat(00, document.body.clientWidth);
+          const y = randomFloat(0, window.innerHeight / 2);
+
+          const x2 = randomFloat(100, document.body.clientWidth);
+          const y2 = randomFloat(0, window.innerHeight / 2);
+
+          startDoubleExplosion(x, y);
+          startDoubleExplosion(x2, y2);
+        }, randomFloat(300, 1000));
+
+        bomb_interval2 = setInterval(() => {
+          const x3 = randomFloat(100, document.body.clientWidth);
+          const y3 = randomFloat(0, window.innerHeight / 2);
+
+          const x4 = randomFloat(100, document.body.clientWidth);
+          const y4 = randomFloat(0, window.innerHeight / 2);
+
+          startDoubleExplosion(x3, y3);
+          startDoubleExplosion(x4, y4);
+        }, randomFloat(500, 1500));
+        // }, 50 * 0.1 * 1000);
+        isBombRunning = true;
+      } // end of setting interval
+
+      // if (shouldAudioPlay) {
+      hbdAudio.loop = true;
+      hbdAudio.play();
+      shouldAudioPlay = false;
+      // }
+    }
+  });
 });
 
-
 //=========== Script for Made with Love end ===========
+
+
+console.log(` 
++-+-+-+-+-+ +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+
+|H|a|p|p|y| |B|i|r|t|h|d|a|y| |S|h|u|b|h|i|
++-+-+-+-+-+ +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+
+`)
+
+console.log(` 
++-+-+-+-+-+ +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+
+|H|a|p|p|y| |B|i|r|t|h|d|a|y| |S|h|u|b|h|i|
++-+-+-+-+-+ +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+
+`)
+
+console.log(` 
++-+-+-+-+-+ +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+
+|H|a|p|p|y| |B|i|r|t|h|d|a|y| |S|h|u|b|h|i|
++-+-+-+-+-+ +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+
+`)
